38bb28326b9f2e05333422c3856ff043f839b53b
Wang <gavin19950511@gmail.com>
2017-01-20 12:59:07 +0800

    Update GpioSet.py

--- GpioSet.py
+++ GpioSet.py
@@ -5,22 +5,28 @@
 import GPIO24 as gpio
 import json
 
-def SetCreat(JsonData):
-	PinSet = []
-	for d in JsonData:
-		#pin = GpioForLights("PinNum",1)
-		#PinSet.append(d['pin'])
-		PinSet.append(gpio.GpioForLights(d['pin'],1))
-	#for i in PinSet:
-	#	print "PinSet"+i
+class GpioSet:
+	def __init__(self,JsonData):
+		self.Set = self.SetCreat(JsonData)
+		
+	def SetCreat(self,JsonData):
+		PinSet = []
+		for d in JsonData:
+			#pin = GpioForLights("PinNum",1)
+			#PinSet.append(d['pin'])
+			PinSet.append(gpio.GpioForLights(d['pin'],1))
+		#for i in PinSet:
+		#	print "PinSet"+i
 	
-	return PinSet
+		return PinSet
+
+
 
 JsonData = json.loads('[{"id":"0","pin":"7","GPIO":"4","Cost":"20","Device":"風扇"},{"id":"1","pin":"11","GPIO":"17","Cost":"20","Device":"監視器"},{"id":"2","pin":"12","GPIO":"18","Cost":"20","Device":"電燈"}]')
-GpioSet = SetCreat(JsonData)
-for i in GpioSet:
+mainarray = GpioSet(JsonData).Set
+for i in mainarray:
 	i.on()
-for i in GpioSet:
+for i in mainarray:
 	i.clean()
 
 
